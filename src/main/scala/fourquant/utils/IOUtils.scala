c5dacebaa4afee1f62791b13a7bd5cf5f006bd9c
Kevin Mader <kevinmader@gmail.com>
2015-04-17 21:56:05 +0200

    adding new portable object tools

--- src/main/scala/fourquant/utils/IOUtils.scala
+++ src/main/scala/fourquant/utils/IOUtils.scala
@@ -4,6 +4,9 @@ import java.io._
 
 import org.apache.spark.SparkConf
 import org.apache.spark.input.PortableDataStream
+import org.apache.commons.io.IOUtils.toByteArray
+
+import scala.reflect.ClassTag
 
 /**
  * Created by mader on 4/12/15.
@@ -27,6 +30,61 @@ object IOUtils {
 
 
   /**
+   * Finally converting the portableobject architecture into a more usuable design pattern. It
+   * holds
+   * @param coreData the underlying datastructure holding either the serialized object or the
+   *                 useful version, or null
+   * @tparam A the useful type which is not serializable
+   * @tparam B the serializable type which is generally not useful
+   */
+  abstract class PortableObject[A,B <: Serializable](var coreData: Either[A,B])(
+    implicit cta: ClassTag[A], ctb: ClassTag[B])
+    extends Externalizable {
+
+    private[IOUtils] def this()(implicit cta: ClassTag[A], ctb: ClassTag[B]) = this(null)
+
+    lazy val lazyA = coreData match {
+      case Left(a) => a
+      case Right(b) => fromBtoA(b)
+    }
+    lazy val lazyB = coreData match {
+      case Left(a) => fromAtoB(a)
+      case Right(b) => b
+    }
+
+    private[IOUtils] def fromAtoB(a: A): B
+    private[IOUtils] def fromBtoA(b: B): A
+
+    def getUseful(): A = lazyA
+    def getSerializable(): B = lazyB
+
+    override def readExternal(in: ObjectInput): Unit = {
+      coreData = in.readObject() match {
+        case bser: B =>  Right(bser)
+        case _ => throw new RuntimeException("Object "+this.getClass.getCanonicalName+" cannot be" +
+          " deserialized")
+      }
+    }
+
+    override def writeExternal(out: ObjectOutput): Unit = {
+      out.writeObject(getSerializable())
+    }
+  }
+
+  class PortableByteStream(var inValue: Either[ByteArrayInputStream,Array[Byte]])
+    extends PortableObject[ByteArrayInputStream,Array[Byte]](inValue) {
+
+    private[IOUtils] def this() = this(null)
+
+    override private[IOUtils] def fromAtoB(a: ByteArrayInputStream): Array[Byte] =
+      toByteArray(a)
+
+    override private[IOUtils] def fromBtoA(b: Array[Byte]): ByteArrayInputStream =
+      new ByteArrayInputStream(b)
+  }
+
+
+  /**
    * Adds some functionality to portable data stream useful for integrating with SCIFIO and other
    * tools
    * @param pds
@@ -35,8 +93,10 @@ object IOUtils {
     def getTrimmedPath() =
       pds.getPath().replaceAll("file:","")
 
-    def cache(): InputStream =
-      new ByteArrayInputStream(pds.toArray())
+    def getSuffix() = pds.getPath().split("[.]").reverse.headOption
+
+    def cache() = new PortableByteStream(Right(pds.toArray()))
+      //new ByteArrayInputStream(pds.toArray())
 
     def isLocal() = isPathLocal(pds.getTrimmedPath())
 
